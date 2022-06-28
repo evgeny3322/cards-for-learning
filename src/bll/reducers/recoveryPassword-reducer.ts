764188bbc2ef8d965a5260e1a67f82362eb7559a
evgeny3322 <retard_x_98@mail.ru>
2022-06-28 17:15:42 +0300

    add component recoveryPass

--- src/bll/reducers/recoveryPassword-reducer.ts
+++ src/bll/reducers/recoveryPassword-reducer.ts
@@ -1,6 +1,55 @@
-export const recoveryPasswordReducer = (state: any = {}, action: any): any => {
-    switch (action) {
+import {authApi} from '../../api/auth-api';
+import {setAppError, setLoadingStatus} from './app-reducer';
+import {DispatchActionType, ThunkType} from "../store";
+
+// type InitStateType = {
+//     info: string,
+//     enteredEmail: string
+// }
+type InitStateType = typeof initState;
+type SetResponseInfoRecoveryPassword = ReturnType<typeof setResponseInfoRecoveryPassword>
+type redirectActionType = ReturnType<typeof redirectEmail>;
+
+const initState = {
+    info: '',
+    enteredEmail: '',
+};
+
+export type RecoveryPasswordActionsType =
+    | SetResponseInfoRecoveryPassword
+    | redirectActionType
+
+export const recoveryPasswordReducer = (state: InitStateType = initState, action: RecoveryPasswordActionsType): InitStateType => {
+    switch (action.type) {
+        case 'rp/SET-RESPONSE-INFO':
+            return {...state, info: action.info}
+        case 'rp/REDIRECT-TO-CHECK-EMAIL-SUCCESS-PAGE':
+            return {...state, enteredEmail: action.enteredEmail};
         default:
-            return state;
+            return state
     }
 }
+
+const setResponseInfoRecoveryPassword = (info: string) =>
+    ({type: 'rp/SET-RESPONSE-INFO', info} as const)
+
+export const redirectEmail = (enteredEmail: string) =>
+    ({type: 'rp/REDIRECT-TO-CHECK-EMAIL-SUCCESS-PAGE',
+    enteredEmail
+} as const);
+
+export const sendPasswordRecovery = (email: string): ThunkType => async (dispatch: DispatchActionType) => {
+    try {
+        dispatch(setLoadingStatus('loading'))
+        const res = await authApi.recoveryPassword(email)
+        dispatch(setResponseInfoRecoveryPassword(res.data.info))
+    } catch (e: any) {
+        const error = e.response ? e.response.data.error : (e.message + ', more details in the console');
+        dispatch(setAppError(error))
+    } finally {
+        dispatch(setLoadingStatus('idle'))
+    }
+}
+
+
+
