d894fd918fb174a16772e70f38193243e2c6c547
evgeny3322 <retard_x_98@mail.ru>
2022-07-10 17:35:36 +0300

    fix link

--- src/api/auth-api.ts
+++ src/api/auth-api.ts
@@ -10,8 +10,8 @@ export const instanceHeroku = axios.create({
 
 
 export const instance = axios.create({
-    baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
-    // baseURL: 'https://neko-back.herokuapp.com/2.0',
+    // baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
+    baseURL: 'https://neko-back.herokuapp.com/2.0',
     withCredentials: true,
 })
 
